0426a9c62ca0fde91753b823156fba8ee9d16793
Gilang Ramadhan <45790050+9Alpha9@users.noreply.github.com>
2024-05-13 09:39:09 +0700

    fix: SEO Metadata

--- app/layout.js
+++ app/layout.js
@@ -9,12 +9,19 @@ const inter = Inter({ subsets: ["latin"] });
 <link rel="icon" href="/favicon.ico?<generated>" sizes="32x32" />;
 
 export const metadata = {
-  title: "Bagian",
-  description:
-    "Bagian adalah salah satu softwarehouse yang memiliki kredible dalam proses pembuatan website, aplikasi berbasis website, UI/UX website. Kami berlokasi pada Kota Sidoarjo. ",
-  keyword:
-    "Softwarehouse, Pembuatan Website, Website, UI, UI/UX, Aplikasi Berbasis Website.",
-  author: "Bagian.net",
+  title: "Bagian | Software House",
+  description: [
+    "Bagian adalah salah satu softwarehouse yang memiliki kredible dalam proses pembuatan website, aplikasi berbasis website, UI/UX website. Kami berlokasi di Kota Sidoarjo.",
+  ],
+  keywords: [
+    "Softwarehouse, Pembuatan Website, Website, UI, UI/UX, Aplikasi Berbasis Website, Bagian Softwarehouse, Bagian.net Bagian.",
+  ],
+  authors: [
+    {
+      name: "Bagian.net",
+      url: "https://www.bagian.net",
+    },
+  ],
   robots: "follow, index",
 };
 
