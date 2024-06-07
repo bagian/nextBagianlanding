0758fda77a6f4ce26ee31aff63af18b9ea63339c
Gilang Ramadhan <45790050+9Alpha9@users.noreply.github.com>
2024-06-08 00:37:03 +0700

    fix: /Link on Headers/Nav/Link/index.jsx

--- app/components/Headers/Nav/Link/index.jsx
+++ app/components/Headers/Nav/Link/index.jsx
@@ -4,6 +4,8 @@ import { motion } from "framer-motion";
 import { slide, scale } from "../../animated";
 
 export default function Index({ data, isActive, setSelectedIndicator }) {
+  if (!data) return null;
+
   const { title, href, index } = data;
 
   return (
