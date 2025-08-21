fdce663d9d838e05b314083cb67c75c7e5561af0
Joey R <bagian.desk@gmail.com>
2025-08-21 21:09:49 +0700

    Fix: Metadata og-image

--- app/layout.js
+++ app/layout.js
@@ -15,7 +15,7 @@ export const openGraphImage = {
   url: bagianOg.src,
   width: 1200,
   height: 630,
-  alt: "Bagian.net",
+  alt: "Bagian.web.id",
   type: "image/png",
 };
 // Menyiapkan metadata untuk SEO dan sharing
@@ -29,34 +29,28 @@ export const metadata = {
   title: "Bagian - Software House",
   description:
     "Bagian adalah salah satu Software House yang memiliki kredibilitas tinggi dalam proses pembuatan website, aplikasi berbasis web, dan UI/UX design.",
-  keywords:
-    "software house, pembuatan website, aplikasi web, UI/UX design, Bagian, bagian corp, software house, aplikasi website, pembuatan website, website sekolah, custom website, maintenance website.",
-  authors: [
-    {
-      name: "Bagian.net",
-      url: "https://www.bagian.net",
-    },
-  ],
   openGraph: {
     title: "Bagian - Software House",
     description:
       "Bagian adalah salah satu Software House yang memiliki kredibilitas tinggi dalam proses pembuatan website, aplikasi berbasis web, dan UI/UX design.",
-    image: openGraphImage.url,
-    url: "https://www.bagian.net",
+    url: "https://www.bagian.web.id",
+    siteName: "Bagian",
+    images: [
+      {
+        url: "https://www.bagian.web.id/og-image.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Bagian - Software House",
+      },
+    ],
     type: "website",
   },
-  metaTags: {
-    "og:title": "Bagian - Software House",
-    "og:description":
+  twitter: {
+    card: "summary_large_image",
+    title: "Bagian - Software House",
+    description:
       "Bagian adalah salah satu Software House yang memiliki kredibilitas tinggi dalam proses pembuatan website, aplikasi berbasis web, dan UI/UX design.",
-    "og:image": openGraphImage.url,
-    "og:url": "https://www.bagian.net",
-    "og:type": "website",
-    "twitter:card": "summary_large_image",
-    "twitter:title": "Bagian - Software House",
-    "twitter:description":
-      "Bagian adalah salah satu Software House yang memiliki kredibilitas tinggi dalam proses pembuatan website, aplikasi berbasis web, dan UI/UX design.",
-    "twitter:image": openGraphImage.url,
+    images: ["https://www.bagian.web.id/og-image.jpg"],
   },
 };
 
